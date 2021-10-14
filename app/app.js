f9b2c36478bea196c0351762075149842410d512
TaeUk471 <asky990223@gmail.com>
2021-10-14 23:53:46 +0900

    nodemon 적용

--- app/app.js
+++ app/app.js
@@ -8,6 +8,7 @@ const PORT = 3000;
 // 앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
 
 const home = require("./src/routes/home"); //routes로 가고 home안에있는 자바스크립트 파일을 읽어와줘.
 app.use("/", home); //use -> 미들 웨어를 등록해주는 메서드.

38e6db14fee1679dbeffeae541c1903ae53ca96c
TaeUk471 <asky990223@gmail.com>
2021-10-14 19:52:59 +0900

    폴더구조 최적화

--- app/app.js
+++ app/app.js
@@ -6,10 +6,10 @@ const app = express();
 const PORT = 3000;
 
 // 앱 세팅
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
-const home = require("./routes/home"); //routes로 가고 home안에있는 자바스크립트 파일을 읽어와줘.
+const home = require("./src/routes/home"); //routes로 가고 home안에있는 자바스크립트 파일을 읽어와줘.
 app.use("/", home); //use -> 미들 웨어를 등록해주는 메서드.
 
 module.exports = app;

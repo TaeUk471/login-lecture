d9ff2ac4ad01c0c3d0e81df655ccb58dd94a17d5
TaeUk471 <asky990223@gmail.com>
2021-10-15 06:38:58 +0900

    User 모델 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -18,6 +18,17 @@ class UserStorage{
         return newUsers;
         
     }
+
+    static getUserInfo(id){
+        const users = this.#users;
+        const idx = users.id.indexOf(id);
+        const userInfo = Object.keys(users).reduce((newUser, info)=>{ // => id ,psword, name
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {});
+        
+        return userInfo;
+    }
 }
 
 module.exports = UserStorage;
